98546aaf7d751398ae95f9eb5640bf0cca962377
Wesley El <48174882+wesleyel@users.noreply.github.com>
2025-03-04 01:52:32 +0000

    Add Docker runner

---
+++ docker/docker_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export DOCKER_BUILDKIT=1
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+PROJECT_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
+
+pushd "$PROJECT_DIR"
+
+docker build -t cloudmark-app -f docker/Dockerfile . && \
+docker run -it --name cloudmark-container --rm -p 3000:3000 cloudmark-app
+
+popd
